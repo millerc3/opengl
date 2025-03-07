33fa3f8f7d77601fcc90cc63293ce0b55f9c162b
Chris <cmiller9713@yahoo.com>
2025-03-06 20:42:59 -0500

    initial commit

---
+++ First/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+
+int main() {
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW Window" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+	glfwMakeContextCurrent(window);
+	
+	// Get the address of the current openGL Function pointer (after setting the context above)
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return 1;
+	}
+
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	return 0;
+}
