752cc4bb3c91b8bad9bcd3d90d19f89659e52388
Chris <cmiller9713@yahoo.com>
2025-03-08 17:14:50 -0500

    updated OpenGLTemplate with helper functions created during HelloTriangle Exercise 2

--- OpenGLTemplate/include/utils/shader_loader.h
+++ OpenGLTemplate/include/utils/shader_loader.h
@@ -1,5 +1,8 @@
 #pragma once
 
 #include <string>
+#include <vector>
+#include <optional>
 
-std::string LoadShaderSource(const std::string& filepath);
+std::optional<GLuint> LoadShaderFromFile(const std::string& filepath, GLenum shaderType);
+std::optional<GLuint> CreateShaderProgram(std::vector<GLuint> shaderIDs);
