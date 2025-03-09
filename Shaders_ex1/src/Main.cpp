911312679bae01e112abf33523ba51568dbf2046
Chris <cmiller9713@yahoo.com>
2025-03-09 15:47:27 -0400

    finished chapter 6 and execises

---
+++ Shaders_ex1/src/Main.cpp
@@ -0,0 +1,148 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <optional>
+#include <vector>
+
+#include <Shader.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void PlaceVertices(std::vector<float>& vertices, unsigned int& VBO, unsigned int& VAO) {
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    Shader shader("src/shaders/shader.vert", "src/shaders/color_passthru.frag");
+
+    std::vector<float> vertices_left = {
+        // first triangle
+        // positions        // colors
+        0.0f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // left 
+        0.9f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // right
+        0.45f, 0.5f, 0.0f,  0.0f, 0.0f, 1.0f // top 
+    };
+
+    // exercise 2: have the same vertices for 2 triangles, but set a uniform to move the second one
+    std::vector<float> vertices_right = {
+        // second triangle
+        // positions        // colors
+        0.0f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // left 
+        0.9f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // right
+        0.45f, 0.5f, 0.0f,  0.0f, 0.0f, 1.0f // top 
+    };
+
+    unsigned int VBOs[2], VAOs[2];
+
+    PlaceVertices(vertices_left, VBOs[0], VAOs[0]);
+
+    PlaceVertices(vertices_right, VBOs[1], VAOs[1]);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        int vertexColorLocation = glGetUniformLocation(shader.ID, "offset");
+
+        shader.use();
+        glUniform3f(vertexColorLocation, 0.0f, 0.0f, 0.0f);
+        glBindVertexArray(VAOs[0]);
+        glDrawArrays(GL_TRIANGLES, 0, vertices_left.size() / 3);
+       
+        glUniform3f(vertexColorLocation, .9f, 0.0f, 0.0f);
+        glBindVertexArray(VAOs[1]);
+        glDrawArrays(GL_TRIANGLES, 0, vertices_right.size() / 3);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(2, VAOs);
+    glDeleteBuffers(2, VBOs);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
